b4fa6cd5bfd82c630948e9e2f4ae072184ba1085
alpine12 <iqballecip@gmail.com>
2020-11-30 21:24:43 +0700

    Finishing Music Service

--- app/src/main/java/com/alpine12/spotifyclone/other/Constant.kt
+++ app/src/main/java/com/alpine12/spotifyclone/other/Constant.kt
@@ -5,5 +5,6 @@ object Constant {
     const val SONG_COLLECTION= "songs"
     const val NOTIFICATION_CHANEL_ID = "music"
     const val NOTIFICATION_ID = 1
+    const val MEDIA_ROOT_ID = "root_id"
 
 }
