0c194720cf1f1444168864377c518721d3433c85
alpine12 <iqballecip@gmail.com>
2021-01-29 21:16:53 +0700

    Setting Update SeekBar

--- app/src/main/java/com/alpine12/spotifyclone/ui/fragment/SongFragment.kt
+++ app/src/main/java/com/alpine12/spotifyclone/ui/fragment/SongFragment.kt
@@ -1,12 +1,15 @@
 package com.alpine12.spotifyclone.ui.fragment
 
 import android.os.Bundle
+import android.support.v4.media.session.PlaybackStateCompat
 import android.view.View
+import android.widget.SeekBar
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import com.alpine12.spotifyclone.R
 import com.alpine12.spotifyclone.data.entities.Song
+import com.alpine12.spotifyclone.exoplayer.isPlaying
 import com.alpine12.spotifyclone.exoplayer.toSong
 import com.alpine12.spotifyclone.other.Status
 import com.alpine12.spotifyclone.ui.viewmodel.MainViewModel
@@ -14,6 +17,9 @@ import com.alpine12.spotifyclone.ui.viewmodel.SongVIewModel
 import com.bumptech.glide.RequestManager
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_song.*
+import timber.log.Timber
+import java.text.SimpleDateFormat
+import java.util.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -28,11 +34,48 @@ class SongFragment : Fragment(R.layout.fragment_song) {
 
     private var curPlayingSong: Song? = null
 
+    private var playbackState: PlaybackStateCompat? = null
+
+    private var shouldUpdateSeekbar = true
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         mainViewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
         subscribeToObservers()
+
+        ivPlayPauseDetail.setOnClickListener {
+            curPlayingSong?.let {
+                mainViewModel.playOrToggleSong(it, true)
+            }
+        }
+
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, progress: Int, fromUser: Boolean) {
+                if (fromUser) {
+                    setCurPlayerTimeToTextVIew(progress.toLong())
+                }
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+                shouldUpdateSeekbar = false
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+                seekBar?.let {
+                    mainViewModel.seekTo(it.progress.toLong())
+                    shouldUpdateSeekbar = true
+                }
+            }
+        })
+
+        ivSkipPrevious.setOnClickListener {
+            mainViewModel.skipToNextPrevious()
+        }
+
+        ivSkip.setOnClickListener {
+            mainViewModel.skipToNextSong()
+        }
     }
 
     private fun updateTitleSongImage(song: Song) {
@@ -47,7 +90,7 @@ class SongFragment : Fragment(R.layout.fragment_song) {
                 when (result.status) {
                     Status.SUCCESS -> {
                         result.data?.let { songs ->
-                            if (curPlayingSong == null && songs.isNotEmpty()){
+                            if (curPlayingSong == null && songs.isNotEmpty()) {
                                 curPlayingSong = songs[0]
                                 updateTitleSongImage(songs[0])
                             }
@@ -58,12 +101,39 @@ class SongFragment : Fragment(R.layout.fragment_song) {
             }
         }
 
-        mainViewModel.curPlayingSong.observe(viewLifecycleOwner){
+        mainViewModel.curPlayingSong.observe(viewLifecycleOwner) {
             if (it == null) return@observe
             curPlayingSong = it.toSong()
             updateTitleSongImage(curPlayingSong!!)
         }
 
+        mainViewModel.playbackStates.observe(viewLifecycleOwner) {
+            playbackState = it
+            ivPlayPauseDetail.setImageResource(
+                if (playbackState?.isPlaying == true) R.drawable.ic_pause else R.drawable.ic_play
+            )
+            seekBar.progress = it?.position?.toInt() ?: 0
+        }
+        songViewModel.curPlayerPosition.observe(viewLifecycleOwner) {
+            Timber.d("time ${it.toInt()}")
+            seekBar.progress = it.toInt()
+            setCurPlayerTimeToTextVIew(it)
+            if (shouldUpdateSeekbar) {
+                seekBar.progress = it.toInt()
+                setCurPlayerTimeToTextVIew(it)
+                Timber.d("time ${it.toInt()}")
+            }
+        }
+        songViewModel.curSongDuration.observe(viewLifecycleOwner) {
+            seekBar.max = it.toInt()
+            val dateFormat = SimpleDateFormat("mm:ss", Locale.getDefault())
+            tvSongDuration.text = dateFormat.format(it)
+        }
+    }
+
+    private fun setCurPlayerTimeToTextVIew(ms: Long) {
+        val dateFormat = SimpleDateFormat("mm:ss", Locale.getDefault())
+        tvCurTime.text = dateFormat.format(ms)
     }
 
 }
